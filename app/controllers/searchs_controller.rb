10bbabcff6b21130d69e9303ada32598f38850d7
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-08-07 15:30:49 +0000

    break

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,50 @@
+class SearchsController < ApplicationController
+  
+  def search
+    # viewのform_tagにて
+    # 選択したmodelの値を@modelに代入。
+    @model = params["model"]
+    # 選択した検索方法の値を@methodに代入。
+    @method = params["method"]
+    # 検索ワードを@contentに代入。
+    @content = params["content"]
+    # @model, @content, @methodを代入した、
+    # search_forを@recordsに代入。
+    @records = search_for(@model, @content, @method)
+  end
+  
+  private
+  def search_for(model, content, method)
+    # 選択したモデルがuserだったら
+    if model == 'user'
+      # 選択した検索方法がが完全一致だったら
+      if method == 'perfect'
+        User.where(name: content)
+      # 選択した検索方法がが部分一致だったら
+      elsif method == 'part'
+        User.where('name LIKE ?', '%'+content+'%')
+      #前方一致
+      elsif method == 'front'
+        User.where('neme LIKE ?', '%'+content+'%')
+      #後方一致  
+      elsif method == 'back'
+        User.where('name LIKE ?', '%'+content+'%')
+        
+      end
+    # 選択したモデルがpostだったら
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      elsif method == 'part'
+        Book.where('title LIKE ?', '%'+content+'%')
+      #前方一致
+      elsif method == 'front'
+        Book.where('title LIKE ?', '%'+content+'%')
+      #後方一致  
+      elsif method == 'back'
+        Book.where('title LIKE ?', '%'+content+'%')
+        
+      end
+    end
+  end
+end
